758dc80a8800965d3ec9b4e22779867a0e056c23
dipu-726 <dipusaha726@gmail.com>
2020-03-20 14:51:48 +0530

    structured file

---
+++ fluxible-poc/src/components/AddName.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+class AddName extends Component{
+    render(){
+        return(
+            <div>
+                 <input type="text" placeholder="Enter the name " /> 
+                 <button onClick={}>Change name </button>
+            </div>
+        )
+    }
+}
+export default AddName;
--- fluxible-poc/src/components/Body.jsx
+++ fluxible-poc/src/components/Body.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
+import AddName from "./AddName";
 class Body extends Component{
     render()
     {
         return(
             <div className="application-body" >
-                <input type="text" placeholder="Enter the name " /> 
-                <button>Change name </button>
+                 <AddName />
                  <p>All Names : </p>
                  <p>Current Name : </p>
-
             </div>
         )
     }    
