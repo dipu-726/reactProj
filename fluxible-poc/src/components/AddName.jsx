134ed57dcd08767fe597f8c57eef206e1840b842
dipu-726 <dipusaha726@gmail.com>
2020-03-20 15:10:17 +0530

    implemented AddName component

--- fluxible-poc/src/components/AddName.jsx
+++ fluxible-poc/src/components/AddName.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 class AddName extends Component{
+
+    handleClick = ()=>{
+       const textFromUser = document.getElementById("addname");
+       console.log("button is called !",textFromUser.value);
+    }
+
     render(){
         return(
             <div>
-                 <input type="text" placeholder="Enter the name " /> 
-                 <button onClick={}>Change name </button>
+                 <input id="addname" type="text" placeholder="Enter the name" /> 
+                 <button onClick={this.handleClick}>Change name </button>
             </div>
         )
     }
